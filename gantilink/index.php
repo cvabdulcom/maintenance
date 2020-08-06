b6d7fbb9de6e3198c7eb49dee0cc6857f9f6defd
Abdul Aziz <cvabdulcom@gmail.com>
2020-08-06 14:22:40 +0700

    pertama commit

---
+++ gantilink/index.php
@@ -0,0 +1,91 @@
+<html lang="en"><head>
+<title>Perbaikan Sistem</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="icon" type="image/png" href="https://colorlib.com/etc/cs/comingsoon_04/images/icons/favicon.ico">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_04/vendor/bootstrap/css/bootstrap.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_04/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_04/fonts/iconic/css/material-design-iconic-font.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_04/vendor/animate/animate.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_04/vendor/select2/select2.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_04/css/util.css">
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_04/css/main.css">
+
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+</head>
+<body cz-shortcut-listen="true">
+<div class="bg-g1 size1 flex-w flex-col-c-sb p-l-15 p-r-15 p-t-55 p-b-35 respon1">
+<div class="flex-col-c p-t-50 p-b-50">
+<h3 class="l1-txt1 txt-center p-b-10">
+Ganti Link
+</h3>
+<a href="http://sicak.geloragroup.com">
+<font size="3" color="white">
+http://sicak.geloragroup.com
+</font>
+</a>
+<br><br>
+<button class="flex-c-m s1-txt2 size3 how-btn" data-toggle="modal" data-target="#subscribe">
+IT Officer&nbsp;<i class="fa fa-heart">&nbsp;</i>Abdul Aziz
+</button>
+</div>
+<span class="s1-txt3 txt-center">
+@ 2020 Gelora Group
+</span>
+</div>
+
+<script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script>
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/jquery/jquery-3.2.1.min.js" type="text/javascript"></script>
+
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/bootstrap/js/popper.js" type="text/javascript"></script>
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/select2/select2.min.js" type="text/javascript"></script>
+
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/countdowntime/moment.min.js" type="text/javascript"></script>
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/countdowntime/moment-timezone.min.js" type="text/javascript"></script>
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/countdowntime/moment-timezone-with-data.min.js" type="text/javascript"></script>
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/countdowntime/countdowntime.js" type="text/javascript"></script>
+<script type="text/javascript">
+		$('.cd100').countdown100({
+			// Set Endtime here
+			// Endtime must be > current time
+			endtimeYear: 0,
+			endtimeMonth: 0,
+			endtimeDate: 3,
+			endtimeHours: 18,
+			endtimeMinutes: 0,
+			endtimeSeconds: 0,
+			timeZone: "" 
+			// ex:  timeZone: "America/New_York", can be empty
+			// go to " http://momentjs.com/timezone/ " to get timezone
+		});
+	</script>
+
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/tilt/tilt.jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+
+<script src="https://colorlib.com/etc/cs/comingsoon_04/js/main.js" type="text/javascript"></script>
+
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13" type="text/javascript"></script>
+<script type="text/javascript">
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+
+</body></html>
---
+++ index.php
@@ -0,0 +1,107 @@
+<html lang="en"><head>
+<title>Perbaikan Sistem</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="icon" type="image/png" href="https://colorlib.com/etc/cs/comingsoon_04/images/icons/favicon.ico">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_04/vendor/bootstrap/css/bootstrap.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_04/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_04/fonts/iconic/css/material-design-iconic-font.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_04/vendor/animate/animate.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_04/vendor/select2/select2.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_04/css/util.css">
+<link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cs/comingsoon_04/css/main.css">
+
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+</head>
+<body cz-shortcut-listen="true">
+<div class="bg-g1 size1 flex-w flex-col-c-sb p-l-15 p-r-15 p-t-55 p-b-35 respon1">
+<span></span>
+<div class="flex-col-c p-t-50 p-b-50">
+<h3 class="l1-txt1 txt-center p-b-10">
+Coming Soon
+</h3>
+<p class="txt-center l1-txt2 p-b-60">
+Sistem Sedang Dalam Perbaikan
+</p>
+<div class="flex-w flex-c cd100 p-b-82">
+<div class="flex-col-c-m size2 how-countdown">
+<span class="l1-txt3 p-b-9 days">35</span>
+<span class="s1-txt1">Days</span>
+</div>
+<div class="flex-col-c-m size2 how-countdown">
+<span class="l1-txt3 p-b-9 hours">17</span>
+<span class="s1-txt1">Hours</span>
+</div>
+<div class="flex-col-c-m size2 how-countdown">
+<span class="l1-txt3 p-b-9 minutes">59</span>
+<span class="s1-txt1">Minutes</span>
+</div>
+<div class="flex-col-c-m size2 how-countdown">
+<span class="l1-txt3 p-b-9 seconds">42</span>
+<span class="s1-txt1">Seconds</span>
+</div>
+</div>
+<button class="flex-c-m s1-txt2 size3 how-btn" data-toggle="modal" data-target="#subscribe">
+IT Officer&nbsp;<i class="fa fa-heart">&nbsp;</i>Abdul Aziz
+</button>
+</div>
+<span class="s1-txt3 txt-center">
+@ 2020 Gelora Group
+</span>
+</div>
+
+<script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script>
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/jquery/jquery-3.2.1.min.js" type="text/javascript"></script>
+
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/bootstrap/js/popper.js" type="text/javascript"></script>
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/select2/select2.min.js" type="text/javascript"></script>
+
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/countdowntime/moment.min.js" type="text/javascript"></script>
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/countdowntime/moment-timezone.min.js" type="text/javascript"></script>
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/countdowntime/moment-timezone-with-data.min.js" type="text/javascript"></script>
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/countdowntime/countdowntime.js" type="text/javascript"></script>
+<script type="text/javascript">
+		$('.cd100').countdown100({
+			// Set Endtime here
+			// Endtime must be > current time
+			endtimeYear: 0,
+			endtimeMonth: 0,
+			endtimeDate: 30,
+			endtimeHours: 18,
+			endtimeMinutes: 0,
+			endtimeSeconds: 0,
+			timeZone: "" 
+			// ex:  timeZone: "America/New_York", can be empty
+			// go to " http://momentjs.com/timezone/ " to get timezone
+		});
+	</script>
+
+<script src="https://colorlib.com/etc/cs/comingsoon_04/vendor/tilt/tilt.jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+
+<script src="https://colorlib.com/etc/cs/comingsoon_04/js/main.js" type="text/javascript"></script>
+
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13" type="text/javascript"></script>
+<script type="text/javascript">
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+
+</body></html>
